bcd432e7904fe8e6af655c86f574b8531c42302e
Nicholas de Jong <nickacidegamer@gmail.com>
2024-08-03 08:52:14 +0200

    Cleanup, improve `SearchHandler` data representation

--- mcts/src/c4i/search.rs
+++ mcts/src/c4i/search.rs
@@ -1,18 +1,20 @@
-use crate::{
-    montecarlo::mcts,
-    types::{board::Board, r#move::Move},
-};
 use std::{
-    sync::{atomic::AtomicBool, Arc, Mutex}, thread::{self, JoinHandle}, time::Duration
+    sync::{atomic::AtomicBool, Arc},
+    thread::{self, JoinHandle},
+    time::Duration,
+};
+use {
+    crate::montecarlo::mcts,
+    types::{board::Board, r#move::Move},
 };
 
 type Handle = JoinHandle<Move>;
 
 #[derive(Default)]
 pub struct SearchHandler {
-    should_stop: Arc<Mutex<AtomicBool>>,
+    should_stop: Arc<AtomicBool>,
     handle: Option<Handle>,
-    searching: bool
+    searching: Arc<AtomicBool>,
 }
 
 impl SearchHandler {
@@ -23,41 +25,31 @@ impl SearchHandler {
     }
 
     pub fn search(&mut self, board: Board, time: Duration) {
-        if self.is_searching() { return }
-        self.searching = true;
-        self.should_stop.clone().lock().unwrap().store(false, std::sync::atomic::Ordering::Release);
+        if self.is_searching() {
+            // try again
+            self.search(board, time);
+            return;
+        }
+        self.searching.as_ref().store(true, std::sync::atomic::Ordering::Release);
+        self.should_stop
+            .clone()
+            .store(false, std::sync::atomic::Ordering::Release);
         let should_stop = self.should_stop.clone();
+        let searching = self.searching.clone();
         let handle = thread::spawn(move || {
-            let mv = mcts(&mut board.clone(), time, should_stop);
+            let mv = mcts(&mut board.clone(), time, should_stop, searching);
             mv
         });
         self.handle = Some(handle);
     }
 
     pub fn stop_search(&mut self) {
-        self.should_stop.clone().lock().unwrap().store(true, std::sync::atomic::Ordering::Release);
-        self.searching = false;
+        self.should_stop
+            .clone()
+            .store(true, std::sync::atomic::Ordering::Release);
     }
 
     pub fn is_searching(&self) -> bool {
-        if let Some(handle) = &self.handle {
-            !handle.is_finished()
-        } else {
-            false
-        }
+        self.searching.as_ref().load(std::sync::atomic::Ordering::Acquire)
     }
-
-    // pub fn get_mv(&mut self) -> Option<Move> {
-    //     if let Some(handle) = &mut self.handle {
-    //         println!("getting mv");
-    //         if handle.is_finished() || !self.searching {
-    //             let mv = self.handle.take().unwrap().join().expect("search failed");
-    //             Some(mv)
-    //         } else {
-    //             None
-    //         }
-    //     } else {
-    //         None
-    //     }
-    // }
 }
